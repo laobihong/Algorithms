a8c5165beef8dbc54bf5b4658054f35f59ff0901
XiaomuLin <laobihong@gmail.com>
2017-02-01 23:57:55 -0800

    minor changes

--- maximumDepthofBinaryTree.js
+++ maximumDepthofBinaryTree.js
@@ -1,5 +1,5 @@
 var maxDepth = function(root) {
-    if(root === undefined || root===null){
+    if(root === undefined || root === null){
         return 0;
     }
     return Math.max(maxDepth(root.left),maxDepth(root.right)) + 1;
