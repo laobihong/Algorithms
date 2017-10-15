5625f22c846e3d6d671f00e18e72befa5fca418d
Xiaomu Lin <xl544@cornell.edu>
2017-10-15 16:49:34 -0700

    turn off xor + and

---
+++ AlgorithmTest/src/test/TurnOffTheRightMostSetBit.java
@@ -0,0 +1,28 @@
+package test;
+
+import org.junit.Assert;
+public class TurnOffTheRightMostSetBit {
+
+    public static int and(int n) {
+        return n & (n - 1);
+    }
+    public static int xor(int n) {
+        int mask = 1;
+        while((mask & n) == 0) {
+            mask = mask << 1;
+        }
+        return mask ^ n;
+    }
+    
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            Assert.assertEquals(and(i), xor(i), 0);
+            if (and(i) == xor(i)) {
+                System.out.print(i + " ");
+                System.out.print(and(i) + " ");
+                System.out.println(xor(i));
+            }
+        }
+    }
+
+}
